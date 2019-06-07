0fbf42584fa4e7a3417cab14e935095a5ab7ace3
Kristin Kraemer <kristinhannahk@gmail.com>
2019-06-07 18:14:16 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -11,9 +11,7 @@ class Anagram
   def match(array)
    # array.find {|w| w.split(" ").sort == self.word.split(" ").sort}
    array.each do |ana|
-    ana.split("").sort
-    binding.pry
-    #.match(self.split(" ").sort)
+    ana.split("").sort.match(self.split("").sort)
     return ana
   end
 end
