1ae4f0be17ed757d877c88599c999420d0ba011f
Kristin Kraemer <kristinhannahk@gmail.com>
2019-06-07 18:14:00 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,21 @@
 # Your code goes here!
+require "pry"
+
+class Anagram 
+  attr_accessor :word
+  
+  def initialize(word)
+    @word = word 
+  end 
+  
+  def match(array)
+   # array.find {|w| w.split(" ").sort == self.word.split(" ").sort}
+   array.each do |ana|
+    ana.split("").sort
+    binding.pry
+    #.match(self.split(" ").sort)
+    return ana
+  end
+end
+  
+end 
